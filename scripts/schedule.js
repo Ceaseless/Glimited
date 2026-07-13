6a133d7b73b871dfaa07d1537c883fd248560eb9
Lucks Smith <luckssmith+git@gmail.com>
2026-07-13 14:26:54 +0200

    Changed default schedule length to 10

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -17,7 +17,7 @@ const referenceTimestamp = {
     utcTimestamp: "2026-07-08T00:00:00+09:00"
 };
 
-function generateSchedule(days = 7) {
+function generateSchedule(days = 10) {
     const schedule = [];
 
     const startIndex = quests.indexOf(referenceTimestamp.quest);
